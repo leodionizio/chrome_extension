a7e264fe6fca75f53670268b67fd1ba17c62fb2e
Leonardo Dionizio <leodionizio13@gmail.com>
2018-10-10 16:39:35 -0300

    created chrome extension

---
+++ js/chrome.js
@@ -0,0 +1,20 @@
+
+$('h1').html("Glória a Deux!");
+$('h2').html("Glória a Deux!");
+$('h3').html("Glória a Deux!");
+$('h4').html("Glória a Deux!");
+$('h5').html("Glória a Deux!");
+$('h6').html("Glória a Deux!");
+
+$('span').html("Aleluia Senhor!");
+$('li').html("Aleluia Senhor!");
+
+$('p').html("Eu vou ser presidente, não sei como!");
+
+// $('a').html("Em nome do Senhor Jesus!");
+$('button').html("Em nome do Senhor Jesus!");
+
+$('img').attr(
+  'src',
+  'https://www.rapidonoar.com.br/wp-content/uploads/2018/09/daciolo-presidente.jpg'
+);
